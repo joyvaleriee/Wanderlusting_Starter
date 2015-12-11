e11f8163563ef4014c0a66701b2cd0f563c073c3
Joy Valerie Carrera <JoyValerie@Joys-MacBook-Pro.local>
2015-12-11 00:14:10 -0500

    tried to add cslick carasel and epically failed.,,, bed time zzzz

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,15 @@
 /* CURRENTLY IN: javascript/main.js */
+
+$(document).ready(function(){
+  $('.slick--about').slick({
+    setting-name: setting-value
+  });
+});
+
+
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
+		
