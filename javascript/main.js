13726fc59a384efb7716dfbe4192dcb29cd740cf
Valerie <joyvaleriecarrera@gmail.com>
2015-12-13 19:05:18 -0500

    Updating gh-pages from master branch via GitBot

---
+++ javascript/main.js
@@ -0,0 +1,15 @@
+/* CURRENTLY IN: javascript/main.js */
+
+$(document).ready(function(){
+  $('.slick--about').slick({
+    setting-name: setting-value
+  });
+});
+
+
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
+		
