f26857a3ef3d86d90a6e72fc68e7cd8ac085987b
Joy Valerie Carrera <JoyValerie@Joys-MacBook-Pro.local>
2015-12-22 01:09:10 -0500

    updated map to be responsive

--- javascript/main.js
+++ javascript/main.js
@@ -8,8 +8,8 @@ $('.js-lightbox-open').on('click', function() {
 
 $('.js-close').on('click', function(){
 	$('.backdrop , .lightbox').css('display','none');
-    console.log($("#photos"),'yooooo');
-    $( "#photos" ).empty();
+	console.log($("#photos"),'yooooo');
+	$( "#photos" ).empty();
 
 
 });
@@ -50,56 +50,56 @@ var APIKEY = 'AIzaSyBgmaIJRDjUXihHRhPYVE7c9M50JBAsasg';
 var APIURL = 'https://maps.googleapis.com/maps/api/js?key='+APIKEY+'&callback=initMap'
 DepManager(APIURL,'google')
 .then(function() {
-    var map = GMap.init( 'map', {
-        center: {lat: 40.7127, lng: -74.0059},
-        zoom: 14
-    })
-    .bindEventHandler('click', function( e, methods, map, options, mapSelector ) {
-        // wont show up in actual console, fyi
-        var latLng = methods.getEventLatLng( e );
-        var coord = {
-        	lat: latLng.lat,
-        	lng: latLng.lng,
-        	distance:200
-        }
-        //document.querySelector('.coords').innerHTML = '<div><b>LAT</b> ' + latLng.lat + ',</div><div><b>LNG</b> ' + latLng.lng + '</div>';
-        
-        //Instagram Function to take coordinates from Google Map 
-        // and input into Instagram API 
-        instaSearch.endpoint(
-    'media/search', coord
+	var map = GMap.init( 'map', {
+		center: {lat: 40.7127, lng: -74.0059},
+		zoom: 14
+	})
+	.bindEventHandler('click', function( e, methods, map, options, mapSelector ) {
+		// wont show up in actual console, fyi
+		var latLng = methods.getEventLatLng( e );
+		var coord = {
+			lat: latLng.lat,
+			lng: latLng.lng,
+			distance:200
+		}
+		//document.querySelector('.coords').innerHTML = '<div><b>LAT</b> ' + latLng.lat + ',</div><div><b>LNG</b> ' + latLng.lng + '</div>';
+		
+		//Instagram Function to take coordinates from Google Map 
+		// and input into Instagram API 
+		instaSearch.endpoint(
+	'media/search', coord
 
-    // {
-    //     lat: 40.707840135059854,
-    //     lng: -74.01120185852051,
-    //     distance: 200
-    // },
-    ,
-    function(data){
-    	console.log('fadfdfa', data);
+	// {
+	//     lat: 40.707840135059854,
+	//     lng: -74.01120185852051,
+	//     distance: 200
+	// },
+	,
+	function(data){
+		console.log('fadfdfa', data);
 
 
-        for(x in data.data){
+		for(x in data.data){
 
-     
-            $('#photos').append('<li class=instacontainer><img class=instaphotos style=background-image": src="'+data.data[x].images.low_resolution.url+'"></li>');
+	 
+			$('#photos').append('<li class=instacontainer><img class=instaphotos style=background-image": src="'+data.data[x].images.low_resolution.url+'"></li>');
 
-        }
+		}
 
-            $('.backdrop , .lightbox').css('display','block');
-    },
-    function(error){
-        console.log('failed ', error);
-    }
+			$('.backdrop , .lightbox').css('display','block');
+	},
+	function(error){
+		console.log('failed ', error);
+	}
 );
 
  $()
 
 
 
-    })
-    .bindEventHandler('dragend', function( e, methods, map ) {
-        var newCenter = methods.getCenter( map );
-        document.querySelector('.drags').innerHTML = '<b>New Center</b> ' + newCenter.lat() + ', ' + newCenter.lng();
-    });
+	})
+	.bindEventHandler('dragend', function( e, methods, map ) {
+		var newCenter = methods.getCenter( map );
+		document.querySelector('.drags').innerHTML = '<b>New Center</b> ' + newCenter.lat() + ', ' + newCenter.lng();
+	});
 });
